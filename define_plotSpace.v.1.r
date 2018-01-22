90508b9ac2c72850d93a93c8ffcf0627bffc1090
JDench <34353759+JDench@users.noreply.github.com>
2018-01-22 13:32:36 -0500

    Changed the defaults
    Cahnged ordering default from TRUE to FALSE, found it is more robust to alternate data.

--- define_plotSpace.v.1.r
+++ define_plotSpace.v.1.r
@@ -4,7 +4,7 @@
 # This function will return a list of lists for the ranges at each level.  This function resolves lists by sequentially calling itself through all list items
 # The tmpSpace is used to define what range of values can be permitted, tmpBuffer is used to define the proportion of given space to use as trim/ or not use....
 # The last argument is to querry if we want to order the values being subdivided.
-define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer = 0.1, func_tmpBuffer_max = 0.4, func_tmpBuffer_min=0.2, func_orderDivs = TRUE){
+define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer = 0.1, func_tmpBuffer_max = 0.4, func_tmpBuffer_min=0.2, func_orderDivs = FALSE){
 	# We define what are the divisions of space that we want to pass to each part, we add one so that this forces the division points to be boundaries
 	# between our spaces to be defined for the current func_tmpSpace
 	func_tmpDivs <- if(is.matrix(func_subDivs) || is.data.frame(func_subDivs)){
