8bfd1c2897bb29f3583de4b9fe3d5f61ccc4c994
JDench <34353759+JDench@users.noreply.github.com>
2018-10-23 07:45:44 -0400

    Fixed buffering of space
    Arguments were not passed to sub-calls.
    
    Also added a new argument so that the most inset group of bars can be "glued" side-by-side.
    
    Have partly fixed bringing the names of func_subDivs into the returned lists.  This requires next work.

--- define_plotSpace.v.1.r
+++ define_plotSpace.v.1.r
@@ -4,7 +4,8 @@
 # This function will return a list of lists for the ranges at each level.  This function resolves lists by sequentially calling itself through all list items
 # The tmpSpace is used to define what range of values can be permitted, tmpBuffer is used to define the proportion of given space to use as trim/ or not use....
 # The last argument is to querry if we want to order the values being subdivided.
-define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer = 0.1, func_tmpBuffer_max = 0.4, func_tmpBuffer_min=0.2, func_orderDivs = FALSE){
+define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer = 0.1, func_tmpBuffer_max = 0.4, func_tmpBuffer_min=0.2, 
+							func_orderDivs = FALSE, func_pairsBeside = TRUE){
 	# We define what are the divisions of space that we want to pass to each part, we add one so that this forces the division points to be boundaries
 	# between our spaces to be defined for the current func_tmpSpace
 	func_tmpDivs <- if(is.matrix(func_subDivs) || is.data.frame(func_subDivs)){
@@ -17,9 +18,28 @@ define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer
 								# We find the space that could be occupied
 								func_tmpSpace <- c(func_tmpDivs[func_thisSpace],func_tmpDivs[func_thisSpace+1])
 								# So the actual space we'll return as permissible will be these boundaries, with trim of total space
-								return( c(func_tmpSpace[1] + max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1])))), 
-											func_tmpSpace[2] - max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1]))))) )
-							})	
+								# BUT, if we're supposed to not have paired groups split apart, then we adjust how this value is calculated
+								if (!is.matrix(func_subDivs) && !is.data.frame(func_subDivs) && func_pairsBeside){
+									# Then the only values that get trimmed are the first and last
+									if(func_thisSpace == 1){
+										c(func_tmpSpace[1] + max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1])))), 
+												func_tmpSpace[2])
+									} else if(func_thisSpace == (length(func_tmpDivs)-1)){
+										c(func_tmpSpace[1],
+											func_tmpSpace[2] - max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1])))))
+									} else {
+										func_tmpSpace
+									}
+								} else {
+									return( c(func_tmpSpace[1] + max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1])))), 
+												func_tmpSpace[2] - max(func_tmpBuffer_min, min(func_tmpBuffer_max,(func_tmpBuffer * (func_tmpSpace[2]-func_tmpSpace[1]))))) )
+								}			
+							})
+	names(func_tmpDivs) <- if(is.matrix(func_subDivs) || is.data.frame(func_subDivs)){
+								as.character(unique(func_subDivs[,1]))
+							} else {
+								unique(names(func_subDivs))
+							}
 	# Using the scale of space we've been given, we'll divide it by the nunber of unique instances in the first
 	# column of func_subDivs (or if it's a vector simply by that value alone)
 	if(is.matrix(func_subDivs) || is.data.frame(func_subDivs)){
@@ -31,7 +51,10 @@ define_plotSpace <- function(func_subDivs, func_tmpSpace = c(0,1),func_tmpBuffer
 		func_tmpReturn <- lapply(1:length(unique(func_subDivs[,1])),function(func_thisDiv){ 
 									define_plotSpace(func_subDivs = func_subDivs[func_tmpParts[[func_thisDiv]],-1], 
 													func_tmpSpace  = func_tmpDivs[[func_thisDiv]],
-													func_tmpBuffer = func_tmpBuffer)
+													func_tmpBuffer = func_tmpBuffer,
+													func_tmpBuffer_max = func_tmpBuffer_max,
+													func_tmpBuffer_min = func_tmpBuffer_min,
+													func_orderDivs = func_orderDivs)
 								})
 		return( func_tmpReturn )
 	} else {
